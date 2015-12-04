bf49d1f6c4901ec66f97c4f8bfac528207697810
liugl <lgl_gongzuo@163.com>
2015-12-04 16:47:51 +0800

    修改应用名称和图标

--- src/com/coolweather/app/receiver/AutoUpdateReceiver.java
+++ src/com/coolweather/app/receiver/AutoUpdateReceiver.java
@@ -1,5 +1,18 @@
 package com.coolweather.app.receiver;
 
-public class AutoUpdateReceiver {
+import com.coolweather.app.service.AutoUpdateService;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AutoUpdateReceiver extends BroadcastReceiver{
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		
+		Intent i=new Intent(context,AutoUpdateService.class);
+		context.startService(i);
+	}
 
 }
